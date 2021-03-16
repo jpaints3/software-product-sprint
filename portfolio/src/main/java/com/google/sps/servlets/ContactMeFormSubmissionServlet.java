ee2b663790b54d0e0780aede9c5a4ec5e6827581
Josiah <jopaintsil@gmail.com>
2021-03-16 18:18:19 +0000

    Separate button and Create Submission servlet

---
+++ portfolio/src/main/java/com/google/sps/servlets/ContactMeFormSubmissionServlet.java
@@ -0,0 +1,16 @@
+package com.google.sps.servlets;
+
+import java.io.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+
+@WebServlet("/submitForm")
+public class ContactMeFormSubmissionServlet extends HttpServlet{
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      
+
+  }
+}
