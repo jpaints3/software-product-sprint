16cc7240e4e6763922c40a1d68338db8d165c280
Josiah <jopaintsil@gmail.com>
2021-03-16 21:04:18 +0000

    Add information to datastore entity, with HTML feedback

--- portfolio/src/main/java/com/google/sps/servlets/ContactMeFormSubmissionServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ContactMeFormSubmissionServlet.java
@@ -1,5 +1,10 @@
 package com.google.sps.servlets;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
 import java.io.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
@@ -12,9 +17,28 @@ public class ContactMeFormSubmissionServlet extends HttpServlet{
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+      // get form information
       String name = Jsoup.clean(request.getParameter("name"), Whitelist.none());
       String email = Jsoup.clean(request.getParameter("email"), Whitelist.none());
       String reason = Jsoup.clean(request.getParameter("reason"), Whitelist.none());
 
+      // create new datastore and keyfactory 
+      Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+      KeyFactory keyFactory = datastore.newKeyFactory();
+
+      // add kind submissions to keyfactory
+      keyFactory.setKind("submissions");
+     
+      //take the submission elements and create a new entity object for it 
+      FullEntity submissionEntity =
+        Entity.newBuilder(keyFactory.newKey())
+            .set("e-name", name)
+            .set("e-email", email)
+            .set("e-reason", reason)
+            .build();
+      datastore.put(submissionEntity);
+
+      response.getWriter().println("Sent");
   }
 }
