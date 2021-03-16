befe68aaa4de27df66495ddc5e42cfb1853766a1
Josiah <jopaintsil@gmail.com>
2021-03-16 20:32:22 +0000

    Add Jsoup dependency to pom file

--- portfolio/src/main/java/com/google/sps/servlets/ContactMeFormSubmissionServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ContactMeFormSubmissionServlet.java
@@ -3,14 +3,18 @@ package com.google.sps.servlets;
 import java.io.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 
 
 @WebServlet("/submitForm")
 public class ContactMeFormSubmissionServlet extends HttpServlet{
 
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      String name = Jsoup.clean(request.getParameter("name"), Whitelist.none());
+      String email = Jsoup.clean(request.getParameter("email"), Whitelist.none());
+      String reason = Jsoup.clean(request.getParameter("reason"), Whitelist.none());
 
   }
 }
